e11aa91b0adcc48baee3b2f9f2fea489bd8b8c0b
Leosly <Leonardonigro11@gmail.com>
2024-02-18 10:52:57 -0500

    Gotta fix this later

--- auto_complete/src/main.rs
+++ auto_complete/src/main.rs
@@ -1,8 +1,14 @@
 use std::io;
 use std::fmt;
+use std::ptr::null;
+
 
 fn main() {
-    
+
+    let li1 = vec!['A','P','P','L','E'];
+    let li2 = vec!['A','P','P','L','E','S'];
+    let li3 = vec!['A','P','E'];
+  
     // so we want to define the structure with nodes
 
     // we've defined the basic structure now I want to handel file reading and adding to the structure
@@ -16,12 +22,14 @@ fn main() {
 
     #[derive(Debug)]
     enum Node{
-        Leaf {name: char,
+        Leaf {
+            name: char,
             terminus: bool,},
 
-        Branch {name: char,
+        Branch {
+            name: char,
             terminus: bool,
-            nodes:Box<Node>}
+            nodes: Option<Box<Node>>}
     }
     #[derive(Debug)]
     struct Trie{
@@ -32,21 +40,31 @@ fn main() {
     impl Node {
         // Method to convert a Leaf node into a Branch node
         // needs to be given a Node type object to attach to next element
-        fn convert_to_branch(self, node: Node) -> Node {
+        fn convert_to_branch(self, node:Option<Box<Node>>) -> Node {
             match self {
                 Node::Leaf { name, terminus } => Node::Branch {
                     name,
                     terminus,
-                    nodes: Box::new(node),
+                    nodes: node,
                 },
                 _ => panic!("Cannot convert non-leaf node to branch"),
             }
         }
     }
 
-    let tree: Trie = Trie{
-        nodes: Box::new(Node::Leaf{ name: 'A', terminus:true})
+    let mut tree: Trie = Trie{
+        nodes: Box::new(Node::Leaf{ name: '.', terminus:true, })
     };
+
+
+    *tree.nodes = Node::Branch { name: li1[0], terminus: false, Some(Box::new(Node::Null)) };
+
+    for i in 1..5{
+        let current = li1[i];
+        // if root
+        let newNode = Node::Leaf { name: li1[i], terminus: false};
+
+    }
 
     println!("{:?}", tree);
 }
